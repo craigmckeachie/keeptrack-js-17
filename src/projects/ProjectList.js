1af2f656b31e0f14d0fbba1306a73f1e0ed9fbd9
Craig McKeachie <craigdmckeachie@gmail.com>
2020-10-22 16:16:04 -0400

    lab06

---
+++ src/projects/ProjectList.js
@@ -0,0 +1,15 @@
+import { checkPropTypes } from 'prop-types';
+import React from 'react';
+import { MOCK_PROJECTS } from './MockProjects';
+import PropTypes from 'prop-types';
+import { Project } from './Project';
+
+function ProjectList({ projects }) {
+  return <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>;
+}
+
+ProjectList.propTypes = {
+  projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+};
+
+export default ProjectList;
