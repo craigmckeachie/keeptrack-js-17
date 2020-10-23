46ccf332af578159f3b2d1f3b06ae22e10d7c5ff
Craig McKeachie <craigdmckeachie@gmail.com>
2020-10-22 22:30:02 -0400

    lab18

--- src/projects/ProjectsPage.js
+++ src/projects/ProjectsPage.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
+import React, { useState, useEffect } from 'react';
 import ProjectList from './ProjectList';
+import { projectAPI } from './projectAPI';
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState(MOCK_PROJECTS);
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(undefined);
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(1);
+        setProjects(data);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, []);
+
   const saveProject = (project) => {
     let updatedProjects = projects.map((p) => {
       return p.id === project.id ? project : p;
@@ -13,7 +31,27 @@ function ProjectsPage() {
   return (
     <>
       <h1>Projects</h1>
+
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+
       <ProjectList projects={projects} onSave={saveProject} />
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </>
   );
 }
