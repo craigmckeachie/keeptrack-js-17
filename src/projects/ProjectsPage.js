a04c7ca1a30184746bf2a30bda9fa6ce7e3055cf
Craig McKeachie <craigdmckeachie@gmail.com>
2021-09-21 17:21:46 -0400

    1

--- src/projects/ProjectsPage.js
+++ src/projects/ProjectsPage.js
@@ -1,49 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useProjects } from './projectHooks';
 import ProjectList from './ProjectList';
-import { projectAPI } from './projectAPI';
-import { Project } from './Project';
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(undefined);
-  const [currentPage, setCurrentPage] = useState(1);
+  const { projects, loading, error, setCurrentPage } = useProjects();
 
-  useEffect(() => {
-    async function loadProjects() {
-      setLoading(true);
-      try {
-        const data = await projectAPI.get(currentPage);
-        if (currentPage === 1) {
-          setProjects(data);
-        } else {
-          setProjects((projects) => [...projects, ...data]);
-        }
-      } catch (e) {
-        setError(e.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-    loadProjects();
-  }, [currentPage]);
 
   const handleMoreClick = () => {
     setCurrentPage((currentPage) => currentPage + 1);
   };
 
   const saveProject = (project) => {
-    projectAPI
-      .put(project)
-      .then((updatedProject) => {
-        let updatedProjects = projects.map((p) => {
-          return p.id === project.id ? new Project(updatedProject) : p;
-        });
-        setProjects(updatedProjects);
-      })
-      .catch((e) => {
-        setError(e.message);
-      });
+    // projectAPI
+    //   .put(project)
+    //   .then((updatedProject) => {
+    //     let updatedProjects = projects.map((p) => {
+    //       return p.id === project.id ? new Project(updatedProject) : p;
+    //     });
+    //     setProjects(updatedProjects);
+    //   })
+    //   .catch((e) => {
+    //     setError(e.message);
+    //   });
   };
   return (
     <>
