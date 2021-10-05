519066c56c521f67b8434b5c84b63273ae4eb910
Craig McKeachie <craigdmckeachie@gmail.com>
2021-10-05 19:29:25 -0400

    Revert "render props"
    This reverts commit a72e364de85ac87e1a6f0a9022581528ed51cd06.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import React from 'react';
 import './App.css';
 import ProjectsPage from './projects/ProjectsPage';
 import ProjectPage from './projects/ProjectPage';
-import Layout from './Layout';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom';
 import HomePage from './home/HomePage';
 
 function App() {
   return (
     <Router>
-      <Layout>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" exact className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects/" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/projects" exact component={ProjectsPage} />
           <Route path="/projects/:id" component={ProjectPage} />
         </Switch>
-      </Layout>
+      </div>
     </Router>
   );
 }
