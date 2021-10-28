71988658522f6757f616b6f8eec8d3d943723935
Craig McKeachie <craigdmckeachie@gmail.com>
2021-10-27 21:18:39 -0400

    completed

--- src/projects/ProjectsPage.js
+++ src/projects/ProjectsPage.js
@@ -1,61 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import ProjectList from './ProjectList';
-import { projectAPI } from './projectAPI';
-import { Project } from './Project';
+import React from "react";
+import ProjectList from "./ProjectList";
+
+import { useProjects } from "./projectHooks";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(undefined);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  useEffect(() => {
-    async function loadProjects() {
-      setLoading(true);
-      try {
-        const data = await projectAPI.get(currentPage);
-        if (currentPage === 1) {
-          setProjects(data);
-        } else {
-          setProjects((projects) => [...projects, ...data]);
-        }
-      } catch (e) {
-        setError(e.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-    loadProjects();
-  }, [currentPage]);
+  const {
+    projects,
+    loading,
+    error,
+    setCurrentPage,
+    saveProject,
+    saving,
+    savingError
+  } = useProjects();
 
   const handleMoreClick = () => {
-    setCurrentPage((currentPage) => currentPage + 1);
+    setCurrentPage(currentPage => currentPage + 1);
   };
 
-  const saveProject = (project) => {
-    projectAPI
-      .put(project)
-      .then((updatedProject) => {
-        let updatedProjects = projects.map((p) => {
-          return p.id === project.id ? new Project(updatedProject) : p;
-        });
-        setProjects(updatedProjects);
-      })
-      .catch((e) => {
-        setError(e.message);
-      });
-  };
   return (
     <>
       <h1>Projects</h1>
-
-      {error && (
+      {saving && <span className="toast">Saving...</span>}
+      {(error || savingError) && (
         <div className="row">
           <div className="card large error">
             <section>
               <p>
                 <span className="icon-alert inverse "></span>
-                {error}
+                {error} {savingError}
               </p>
             </section>
           </div>
